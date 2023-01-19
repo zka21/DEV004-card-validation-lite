ceb343f1d7e44dc1e23dc363203657610a031d2c
ZhaidaCazasola <zhaidacazasola.20@gmail.com>
2023-01-19 11:41:41 -0500

    algotimo de Luhn

---
+++ src/main.js
@@ -0,0 +1,57 @@
+//obtener elemento por ID
+
+const numeroTarjeta = document.getElementById("cardnumber"); // input
+const botonValidar = document.getElementById("botonValidar"); // button 
+
+botonValidar.addEventListener("click", isValid);
+numeroTarjeta.addEventListener("input", validarCantidadDeDigitos)
+
+
+function validarCantidadDeDigitos() {
+  // Esta funcion tiene que validad que haya 16 digitos ni mas ni menos
+  //1 saber cuantos digitos tiene numeroTarjeta.value (check)
+  //2 comparar el numero de digitos con 16 (check)
+
+  let digitos = String(numeroTarjeta.value);
+
+  if (digitos.length >= 16) {
+    digitos = digitos.slice(0, 16);
+    numeroTarjeta.value = parseInt(digitos);
+    botonValidar.disabled = false;
+  } else {
+    botonValidar.disabled = true;
+  }
+
+}
+
+
+function isValid() {
+  //ALGORITMO DE LUHN
+  //Que el valor del input se vuelva en un string  
+  const digitos = String(numeroTarjeta.value);
+  const arrayDigitos = []
+  let digitosInvertido = []
+
+  //Que ese string se vuelva en un array 
+  for (let i = 0; i < digitos.length; i++) {
+    arrayDigitos.push(parseInt(digitos[i]));
+  }
+
+  digitosInvertido = arrayDigitos.reverse();
+
+  for (let i = 1;i<=15;i=i+2){
+    let producto=0;
+    let exceso ="";
+    producto=digitosInvertido[i]*2;
+    
+    if (producto>=10) {
+      exceso=String(producto);
+      producto=parseInt(exceso[0])+parseInt(exceso[1]);
+    }
+    digitosInvertido[i]=producto
+    
+  }
+  console.log(digitosInvertido);
+}
+
+
